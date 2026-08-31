c051d1553824e53ede1032bb0cb6701c834c501f
VEDHANNTH I <vedhannth2916@gmail.com>
2026-08-31 11:51:36 +0530

    Time: 2 ms (47.2%), Space: 46.5 MB (26.19%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -0,0 +1,22 @@
+class Solution{
+    public boolean canVisitAllRooms(List<List<Integer>> rooms){
+        int n=rooms.size();
+        boolean[] visited=new boolean[n];
+        Queue<Integer> queue=new LinkedList<>();
+        queue.offer(0);
+        visited[0]=true;
+        while(!queue.isEmpty()){
+            int room=queue.poll();
+            for(int key:rooms.get(room)){
+                if(!visited[key]){
+                    visited[key]=true;
+                    queue.offer(key);
+                }
+            }
+        }
+        for(boolean roomVisited:visited){
+            if(!roomVisited)return false;
+        }
+        return true;
+    }
+}
