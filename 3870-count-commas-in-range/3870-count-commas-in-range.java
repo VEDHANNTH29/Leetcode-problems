ec6e541c4aef0cc514d0c7b45dcc734715b90425
VEDHANNTH I <vedhannth2916@gmail.com>
2026-09-08 09:38:12 +0530

    Time: 4 ms (16.64%), Space: 42.5 MB (72.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,17 +1,8 @@
 class Solution {
-    private int countDigits(int num){
-        int count=0;
-        while(num!=0){
-            count++;
-            num/=10;
-        }
-        return count;
-    }
     public int countCommas(int n) {
         int numberOfDigits=0;
         int ansCount=0;
-        numberOfDigits=countDigits(n);
-        if(numberOfDigits<4)
+        if(n<1000)
            return 0;
         else{
             for(int i=1000;i<=n;i++){
